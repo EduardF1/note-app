6d935b701ce4786c90dfe04fb0a9b433ad60e1e0
EduardF1 <fischer_eduard@yahoo.com>
2021-10-31 21:39:36 +0100

    initial commit

---
+++ notes.md
@@ -0,0 +1,7 @@
+# AWS amplify installation
+- run `npm install -g @aws-amplify/cli` in the directory of the project for which you want to use the AWS CLI.
+# Install React
+- run `npm install -g create-react-app`
+# Resources
+- <a href="https://www.youtube.com/watch?v=hJggzDsLn2M">Video</a>
+- <a href="https://medium.com/open-graphql/create-a-multiuser-graphql-crud-l-app-in-10-minutes-with-the-new-aws-amplify-cli-and-in-a-few-73aef3d49545">Article</a>
--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css'
+import {Button, Card, Col, Input, message, Row, Typography} from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// AWS imports
+import Amplify, {API, graphqlOperation} from 'aws-amplify';
+import {withAuthenticator} from 'aws-amplify-react';
+import aws_exports from './aws-exports'; // specify the location of aws-exports.js file on your project
+
+import * as queries from './graphql/queries';
+import * as mutations from './graphql/mutations';
+import {Component} from "react";
+
+Amplify.configure(aws_exports);
+
+const {Title} = Typography;
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: "",
+            notes: [],
+            value: "",
+            displayAdd: true,
+            displayUpdate: false
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    async componentDidMount() {
+        this.notes = this.listNotes();
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    async listNotes() {
+        try {
+            const notes = await API.graphql(graphqlOperation(queries.listNotes));
+            this.setState({notes: notes.data.listNotes.items});
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async handleSubmit(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        const note = {"note": this.state.value};
+        try {
+            await API.graphql(graphqlOperation(mutations.createNote, {input: note}));
+            message.success('Note added.');
+            await this.listNotes();
+            this.setState({value: ""});
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async handleUpdate(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        const note = {"id": this.state.id, "note": this.state.value};
+        try {
+            await API.graphql(graphqlOperation(mutations.updateNote, {input: note}));
+            this.setState({displayAdd: true, displayUpdate: false, value: ""});
+            await this.listNotes();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    selectNote(note) {
+        this.setState({id: note.id, value: note.note, displayAdd: false, displayUpdate: true});
+    }
+
+    async handleDelete(id) {
+        const noteId = {"id": id};
+        try {
+            await API.graphql(graphqlOperation(mutations.deleteNote, {input: noteId}));
+            message.success('Note deleted.');
+            await this.listNotes();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    render() {
+        const data = [].concat(this.state.notes)
+            .map((item, i) =>
+                <Card key={i} onClick={this.selectNote.bind(this, item)}>
+                    <Row>
+                        <Col span={12}>
+                            <span>{item.note}</span>
+                        </Col>
+                        <Col span={12}>
+                            <Button type="primary" onClick={this.handleDelete.bind(this, item.id)}>Delete Note</Button>
+                        </Col>
+                    </Row>
+                </Card>
+            );
+        return <div>
+            <Row>
+                <Col span={24}>
+                    <Title>Notes</Title>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Input placeholder="Add a new note" value={this.state.value} onChange={this.handleChange}/>
+                </Col>
+                <Col>
+                    {
+                        this.state.displayUpdate ?
+                            <Button type="primary" onClick={this.handleUpdate.bind(this)}>Update Note</Button> :
+                            <Button onClick={this.handleSubmit.bind(this)}>Add Note</Button>
+                    }
+                </Col>
+            </Row>
+            <Row>
+                <hr/>
+                <Col span={24}>
+                    {data}
+                </Col>
+            </Row>
+        </div>;
+    }
 }
 
-export default App;
+export default withAuthenticator(App, {includeGreetings: true});
+// export default App;
